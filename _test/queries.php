4b4ce10a51d628d63c655e541293c0aab800c636
Erik Storla <estorla@spsu.edu>
2014-11-06 18:40:59 -0500

    Adding function to get a user's mix tape

--- _test/queries.php
+++ _test/queries.php
@@ -16,7 +16,7 @@ function initializeConnection() {
 
 function getAllSongs() {
     $con = initializeConnection();
-    
+
     $query = 'SELECT title '
             . 'FROM tbl_song';
     $stmt = $con->prepare($query);
@@ -26,6 +26,25 @@ function getAllSongs() {
     $returnArray = array();
     while ($stmt->fetch()) {
         array_push($returnArray, $song_title);
+    }
+    return $returnArray;
+}
+
+function getUserMixTape($userID) {
+    $con = initializeConnection();
+
+    $query = 'SELECT tbl_song.title, tbl_mixtape.position FROM tbl_song ' .
+            'JOIN tbl_mixtape ON tbl_mixtape.song_id = tbl_song.song_id ' .
+            'WHERE tbl_mixtape.user_id = ? ' .
+            'ORDER BY tbl_mixtape.position;';
+    $stmt = $con->prepare($query);
+
+    $stmt->bind_param('i',$userID);
+    $stmt->execute();
+    $stmt->bind_result($song_title,$song_position);
+    $returnArray = array();
+    while ($stmt->fetch()) {
+        array_push($returnArray, ($song_title.' | '.$song_position));
     }
     return $returnArray;
 }
