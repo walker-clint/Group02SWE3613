5d96e753df3374bd37c32970e8c0b36f3629b31b
Erik Storla <estorla@spsu.edu>
2014-11-06 18:41:45 -0500

    Adding code to demonstrate getUserMixTape()
    Adding code to demonstrate the getUserMixTape() function
    
    Adjusting layout of page

--- _test/test.php
+++ _test/test.php
@@ -4,21 +4,30 @@
     <head>
         <meta charset="UTF-8">
         <title>Test Page</title>
+        <?php include './queries.php'; ?>
+
     </head>
     <body>
-        <h2>Sample query<br></h2>
-        <p>The following demonstrates a simple SQL query via PHP</p>
+        <h1>Sample queries<br></h1>
+        <p>The following demonstrates SQL queries via PHP</p>
         <p>'queries.php' (will) contain the php functions that return the necessary information gathered from an SQL query</p>
         <p>In this way one all the queries (as functions) can be written into one central location.</p>
 
-        <div style="border: solid black 1px">
-            <?php
-            include './queries.php';
-
+        <div>
+            <h2>Get list of all songs as array</h2>
+            <table border="1"><td><?php
             echo var_dump(getAllSongs());
-            ?>
+            ?></td></table>
         </div>
         <p>This is a simple var_dump(ARRAY) which causes the php to spit out an array and it's metadata.  
-            The function 'getAllSongs($con)' returns a php array.</p>
+            The function 'getAllSongs($con)' returns a php array.
+        </p>
+
+        <div>
+            <h2>Get user 1's mix tape</h2>
+            <table border="1"><td><?php
+            echo var_dump(getUserMixTape(1));
+            ?></td>
+        </div>
     </body>
 </html>
