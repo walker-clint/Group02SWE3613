6ff04b9291b9a972b885842ae154a5635fdfefb1
Erik Storla <estorla@spsu.edu>
2014-11-06 18:54:59 -0500

    Minor whitespace

--- _test/loginInfo.php
+++ _test/loginInfo.php
@@ -1,12 +1,11 @@
 <?php
-
 $host = 'swe3613.com';
 $user = 'wapp02p2swe3613';
 $pass = '345dfg567dss';
-$schema = 'swe3613_db02p2';
+$schema='swe3613_db02p2';
 
-$con = mysqli_connect($host, $user, $pass, $schema);
+$con = mysqli_connect($host,$user,$pass,$schema);
 
-if ($con->connect_error) {
+if ($con->connect_error){
     die('<p>$con->connect_errno: $con->connect_error</p>');
 }
