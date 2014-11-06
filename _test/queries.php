8e80043f3998c377c4514064ae2eac52d0a4161a
Erik Storla <estorla@spsu.edu>
2014-11-06 18:21:54 -0500

    Refactoring queries.php
    Putting login info into the queries.php for cleaner method calls,
    removing unnecessary comments
    
    Removed loginInfo.php (redundant now)

--- _test/queries.php
+++ _test/queries.php
@@ -1,8 +1,22 @@
 <?php
 
-include 'loginInfo.php';
+function init() {
+    $host = 'swe3613.com';
+    $user = 'wapp02p2swe3613';
+    $pass = '345dfg567dss';
+    $schema = 'swe3613_db02p2';
 
-function getAllSongs($con) {
+    $con = mysqli_connect($host, $user, $pass, $schema);
+
+    if ($con->connect_error) {
+        die('<p>$con->connect_errno: $con->connect_error</p>');
+    }
+    return $con;
+}
+
+function getAllSongs() {
+    $con = init();
+    
     $query = 'SELECT title '
             . 'FROM tbl_song';
     $stmt = $con->prepare($query);
