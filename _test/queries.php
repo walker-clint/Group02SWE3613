a89135b4969122c84b9d9fc5ea320c90a0a5141a
Erik Storla <estorla@spsu.edu>
2014-11-06 18:10:15 -0500

    Cleaning up queries.php
    Clearing out some excess code

--- _test/queries.php
+++ _test/queries.php
@@ -2,27 +2,16 @@
 
 include 'loginInfo.php';
 
-//function makeStatement($queryString) {
-//    $stmtP = $con->prepare($queryString);
-//    return $stmtP;
-//}
-
 function getAllSongs($con) {
-    //$temp = '';
-    //$stmt = makeStatement('SELECT title FROM tbl_song');
     $query = 'SELECT title '
             . 'FROM tbl_song';
-    //$stmt = makeStatement($query);
     $stmt = $con->prepare($query);
 
-    //return temp;
     $stmt->execute();
     $stmt->bind_result($song_title);
     $returnArray = array();
     while ($stmt->fetch()) {
         array_push($returnArray, $song_title);
-        //$temp.=$song_title . '<br>';
     }
-    //return $temp;
     return $returnArray;
 }
