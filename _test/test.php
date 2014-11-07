3929be6cd9c6efbb59623bed9d9558f6e6c4eb86
Erik Storla <estorla@spsu.edu>
2014-11-07 14:19:09 -0500

    updating examples
    getBestMixTape() example, uses getSong() as well
    
    cleaned up layout of other examples

--- _test/test.php
+++ _test/test.php
@@ -14,25 +14,55 @@
         <p>In this way one all the queries (as functions) can be written into one central location.</p>
 
         <div>
-            <h2>Get list of all songs as array</h2>
+            <h2>Get Best Mix Tape</h2>
+            The best mix tape, the 10 most popular songs<br>
+            getBestMixTape()
             <table border="1">
                 <tr>
-                    <td>(getAllSongs());</td>
-                    <td>Song<br>
-                    ID | Title | App | Flagged? | YouTube | YouTube App</td>
+                    <td>
+                        <?php
+                        $mixTapeList = getBestMixTape();
+                        echo var_dump($mixTapeList);
+                        ?>
+                    </td>
                 </tr>
                 <tr>
                     <td>
                         <?php
-                        echo var_dump(getAllSongs());
+                        foreach ($mixTapeList as $tempSong) {
+                            echo getSong($tempSong) . '<br>';
+                        }
+                        ?>
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+        <div>
+            <h2>Get list of all songs as array</h2>
+            <p>This is a simple var_dump(ARRAY) which causes the php to spit out an array and it's metadata.  
+                The function returns a php array.
+            </p>
+            <table border="1">
+                <tr>
+                    <td>getAllSongs()</td>
+                    <td>
+                        Song<br>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <?php
+                        $songArray = getAllSongs();
+                        echo var_dump($songArray);
                         ?>
                     </td>
                     <td>
                         <?php
-                        $songArray = getAllSongs();
+                        //$songArray = getAllSongs();
                         foreach ($songArray as $incSong) {
                             if ($incSong instanceof Song) {
-                                echo $incSong->printSong() . '<br>';
+                                echo $incSong . '<br>';
                             }
                         }
                         ?>
@@ -40,13 +70,11 @@
                 </tr>
             </table>
         </div>
-        <p>This is a simple var_dump(ARRAY) which causes the php to spit out an array and it's metadata.  
-            The function returns a php array.
-        </p>
+
 
         <div>
             <h2>Get user 1's mix tape</h2>
-            echo var_dump(getUserMixTape(1));
+            getUserMixTape(1)
             <table border="1">
                 <td>
                     <?php
@@ -58,7 +86,7 @@
 
         <div>
             <h2>Get a song's genres</h2>
-            echo var_dump(getSongGenre(1));
+            getSongGenre(1)
             <table border="1">
                 <td>
                     <?php
