9f447de2b13f89e23a65b8b615d313e43e2422f0
Erik Storla <estorla@spsu.edu>
2014-11-10 10:59:31 -0500

    adding connection.php
    Splitting connection creation into its own file
    
    initializeConnection() removed from queries.php

---
+++ php/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+function initializeConnection() {
+    $host = 'swe3613.com';
+    $user = 'wapp02p2swe3613';
+    $pass = '345dfg567dss';
+    $schema = 'swe3613_db02p2';
+
+    $con = mysqli_connect($host, $user, $pass, $schema);
+
+    if ($con->connect_error) {
+        die('<p>$con->connect_errno: $con->connect_error</p>');
+    }
+    return $con;
+}
--- php/queries.php
+++ php/queries.php
@@ -1,20 +1,7 @@
 <?php
 
-//include_once './../php/objects.php';
-
-function initializeConnection() {
-    $host = 'swe3613.com';
-    $user = 'wapp02p2swe3613';
-    $pass = '345dfg567dss';
-    $schema = 'swe3613_db02p2';
-
-    $con = mysqli_connect($host, $user, $pass, $schema);
-
-    if ($con->connect_error) {
-        die('<p>$con->connect_errno: $con->connect_error</p>');
-    }
-    return $con;
-}
+include_once dirname(__FILE__) . './connection.php';
+include_once dirname(__FILE__) . './objects.php';
 
 function getBestMixTape() {
     $con = initializeConnection();
