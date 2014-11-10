7a1c3978ec13ab9892f311ce5203cbb2dccf2676
Erik Storla <estorla@spsu.edu>
2014-11-10 11:19:35 -0500

    setQueries.php, add new artists, genres; add artist/genre to song
    addArtist() and addGenre() add a new record of the applicable type to
    the DB
    
    addSongArtist() and AddSongGenre() add a relation to the song (song X is
    part of Genre Y)

--- php/setQueries.php
+++ php/setQueries.php
@@ -25,3 +25,55 @@ function updateSong($id, $title, $approved, $flagged, $youtubeLink, $youtubeAppr
     $stmt->bind_param('siisii', $title, $approved, $flagged, $youtubeLink, $youtubeApproved, $id);
     $stmt->execute();
 }
+
+function addArtist($name) {
+    $con = initializeConnection();
+
+    $query = 'INSERT INTO tbl_artist(name) values (?)';
+
+    $stmt = $con->prepare($query);
+    $stmt->bind_param('s', $name);
+    $stmt->execute();
+    //$insertId = $con->insert_id;
+    $insertId = mysqli_insert_id($con);
+    return $insertId;
+}
+
+function addGenre($name){
+    $con = initializeConnection();
+
+    $query = 'INSERT INTO tbl_genre(name) values (?)';
+
+    $stmt = $con->prepare($query);
+    $stmt->bind_param('s', $name);
+    $stmt->execute();
+    
+    $insertId = mysqli_insert_id($con);
+    return $insertId;
+}
+
+function addSongArtist($songId, $artistId){
+    $con = initializeConnection();
+
+    $query = 'INSERT INTO tbl_song_artist(song_id, artist_id) values (?,?)';
+
+    $stmt = $con->prepare($query);
+    $stmt->bind_param('ii', $songId, $artistId);
+    $stmt->execute();
+    
+    $insertId = mysqli_insert_id($con);
+    return $insertId;
+}
+
+function addSongGenre($songId, $genreId){
+    $con = initializeConnection();
+
+    $query = 'INSERT INTO tbl_song_genre(song_id, genre_id) values (?,?)';
+
+    $stmt = $con->prepare($query);
+    $stmt->bind_param('ii', $songId, $genreId);
+    $stmt->execute();
+    
+    $insertId = mysqli_insert_id($con);
+    return $insertId;
+}
