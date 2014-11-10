f1b04477f2c31af47d031561e125b24fd8f0b14b
Erik Storla <estorla@spsu.edu>
2014-11-10 11:22:18 -0500

    testSongUpdate.php, initial
    Tests to see if insert and update functions related to songs (and thus
    artists and genres) work

---
+++ _test/testSongUpdate.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        include_once dirname(__FILE__) . '/../php/setQueries.php';
+        include_once dirname(__FILE__) . '/../php/queries.php';
+        include_once dirname(__FILE__) . '/../php/objects.php';
+
+        $title = 'Bohemian Rhapsody';
+        $approved = 1;
+        $flagged = 0;
+        $youtubeLink = 'http://www.youtube.com/watch?v=k-ARuoSFflc';
+        $youtubeApproved = 1;
+
+        $newSongId = addSong($title, $approved, $flagged, $youtubeLink, $youtubeApproved);
+        $newArtist = addArtist('Queen');
+        $newGenre = addGenre('Epic Rock');
+        
+        addSongArtist($newSongId, $newArtist);
+        addSongGenre($newSongId, $newGenre);
+
+        echo $newSongId . '<br>';
+
+        echo getSong($newSongId).'<br>';
+        echo getSong($newSongId)->getArtists().'<br>'.  getSong($newSongId)->getGenres();
+        ?>
+    </body>
+</html>
