c907130a14687cfb4604aada75c171870a12f7e7
Clint Walker <walker.clint@icloud.com>
2014-11-13 14:27:54 -0500

    nav

--- login.php
+++ login.php
@@ -77,6 +77,13 @@ if ($_POST['user_name']) {
     <div id="top-panel" class="container-fluid expanded-panel">
         <div class="row">
             <div id="logo" class="col-xs-2 col-sm-2"><img src="img/cllogo.png" class="img-responsive"/></div>
+            <ul class="nav navbar-nav pull-right panel-menu">
+                <li class="btn-label-right">
+                    <div class="well-1 btn">
+                        <a href="index.html">Home</a>
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
 </header>
