f71f8a6a9cef002283190a923d0a9f9085dccf62
Clint Walker <walker.clint@icloud.com>
2014-11-13 11:20:37 -0500

    test 4

--- register.php
+++ register.php
@@ -108,7 +108,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <!--Start Content-->
     <div class="row">
         <div id="left-column" class="col-sm-3">
-            <div id="logo" class="col-xs-2 col-sm-2"><img src="img/Comic_Characters_Painter_clip_art_medium.png" class="img-responsive"/></div>
+            <div id="logo" class="col-xs-2 col-sm-2"><img src="img/Comic_Characters_Painter_clip_art_hight.png" class="img-responsive"/></div>
 
             Image Credit: <a href="http://vector.me/browse/132175/people_man_artist_painter_comic_characters_painters"
                              title="People Man Artist Painter Comic Characters Painters" target="_blank">People Man
