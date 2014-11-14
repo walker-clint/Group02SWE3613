32d10a70b84fa39dd59c228818cb963cbbf36ba0
Erik Storla <estorla@spsu.edu>
2014-11-14 14:50:50 -0500

    test of new way to do page default elements

---
+++ _test/index_.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php require $_SERVER['DOCUMENT_ROOT'] . '/html/head.html'; ?>
+    </head>
+    <body>
+        <!--Start Header-->
+        <?php require $_SERVER['DOCUMENT_ROOT'] . '/html/header.html'; ?>
+        <!--End Header-->
+        
+        <!--Start Middle-->
+        <div id="main" class="container-fluid">
+
+            <!--Start Content-->
+            <div class="row">
+                <div id="left-column" class="col-sm-4">
+                    <div class="well bs-component">
+                        <!--<legend>LEFT COLUMN</legend>-->
+                        <h1>Left Column</h1>
+
+                        <div class="form-horizontal" action="" method="POST"></div>
+
+                        <div class="well-1 bs-component">
+
+                        </div>
+                    </div>
+                </div>
+
+                <div id="right-column" class="col-sm-8">
+                    <div class="well bs-component">
+                        <!--<legend>RIGHT COLUMN</legend>-->
+                        <h1>Right Column</h1>
+
+                        <div class="form-horizontal" action="" method="POST"></div>
+
+                        <div class="well-1 bs-component">
+
+                            <!--End well-1-->
+                        </div>
+                    </div>
+
+                </div>
+                <!--End Content-->
+
+            </div>
+            <!--End Middle-->
+
+            <!--End Container-->
+            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+            <!--<script src="http://code.jquery.com/jquery.js"></script>-->
+            <script src="plugins/jquery/jquery-2.1.0.min.js"></script>
+            <script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+            <!-- Include all compiled plugins (below), or include individual files as needed -->
+            <script src="plugins/bootstrap/bootstrap.min.js"></script>
+            <script src="plugins/justified-gallery/jquery.justifiedgallery.min.js"></script>
+            <script src="plugins/tinymce/tinymce.min.js"></script>
+            <script src="plugins/tinymce/jquery.tinymce.min.js"></script>
+            <!-- All functions for this theme + document.ready processing -->
+            <script src="js/devoops.js"></script>
+        </div>
+    </body>
+</html>
