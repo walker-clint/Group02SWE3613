a00cc51df182a31aa651d69a524f10f8a81e4f58
Erik Storla <estorla@spsu.edu>
2014-11-15 11:34:04 -0500

    header.php
    changing link to index.php to only be image and not entire <div>
    
    cleaning whitespace

--- _page/header.php
+++ _page/header.php
@@ -1,26 +1,21 @@
-<?php 
+<?php
 $toplinks = "";
 session_start();
 include_once "connect_to_mysql.php";
-if ($_SESSION['id']!="") {
-	$id = $_SESSION['id'];
-	// Put stored session variables into local php variable
+if ($_SESSION['id'] != "") {
+    $id = $_SESSION['id'];
+    // Put stored session variables into local php variable
     $sql = mysql_query("SELECT * FROM tbl_user WHERE user_id='$id'");
-           
-                while ($row = mysql_fetch_array($sql)) {
-                    // Use the AYAH object to see if the user passed or failed the game.
-                    // Get member ID into a session variable
-                    $full_name = $row["first_name"] ." ". $row["last_name"];
-                   
-                } // close while
-            
-	
-	
-	
-	$toplinks = '<ul class="nav navbar-nav pull-right panel-menu">
+
+    while ($row = mysql_fetch_array($sql)) {
+        // Use the AYAH object to see if the user passed or failed the game.
+        // Get member ID into a session variable
+        $full_name = $row["first_name"] . " " . $row["last_name"];
+    } // close while
+    $toplinks = '<ul class="nav navbar-nav pull-right panel-menu">
 	<li class="btn-label-right">
 	<div class="well-1 btn">
-	<a href="main_menu.php">'.$full_name.'</a>
+	<a href="main_menu.php">' . $full_name . '</a>
 	</div>
 	</li>
 	<li class="btn-label-right">
@@ -29,7 +24,7 @@ if ($_SESSION['id']!="") {
 	</li>
 	</ul>';
 } else {
-	$toplinks = '<ul class="nav navbar-nav pull-right panel-menu">
+    $toplinks = '<ul class="nav navbar-nav pull-right panel-menu">
 	<li class="btn-label-right">
 	<div class="well-1 btn">
 	<a href="login.php">Login</a>
@@ -42,14 +37,15 @@ if ($_SESSION['id']!="") {
 	</li>
 	</ul>';
 }
-
 ?>
 <header class="navbar-collapse">
 
-<a href="index.php">
- <div id="logo" class="col-xs-6 col-sm-6"> <img src="img/cllogo_medium.png" class="img-responsive"/> </div>
-</a>
-  <div id="top-panel" class="container-fluid expanded-panel"> <br>
-    <div class="row"><?php echo $toplinks; ?></div>
-  </div>
+
+    <div id="logo" class="col-xs-6 col-sm-6">
+        <a href="index.php"> <img src="img/cllogo_medium.png" class="img-responsive"/></a>
+    </div>
+
+    <div id="top-panel" class="container-fluid expanded-panel"> <br>
+        <div class="row"><?php echo $toplinks; ?></div>
+    </div>
 </header>
