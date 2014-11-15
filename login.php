75cb136d1549efc9ca54cf780442b8b57456406c
Clint Walker <walker.clint@icloud.com>
2014-11-15 16:20:26 -0500

    7

--- login.php
+++ login.php
@@ -57,7 +57,7 @@
                         <!--<legend>LEFT COLUMN</legend>-->
                         <h1 align="center">Login</h1>
                         <div class="well-1 bs-component">
-                            <form class="form-horizontal" action="php/loginService.php" method="POST" name="logform" id="logform"  >
+                            <form class="form-horizontal" action="php/loginService.php" method="POST">
                                 <font color="#FF0000"><?php echo "$errorMsg"; ?></font> <br>
                                 <div class="form-group">
                                     <label for="user_name" class="col-lg-4 control-label">User Name</label>
