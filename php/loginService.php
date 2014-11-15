ae9be7f4576b0ac3060b7f1570edfea03efa22c3
Erik Storla <estorla@spsu.edu>
2014-11-15 17:35:01 -0500

    loginService.php

--- php/loginService.php
+++ php/loginService.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //include('php/Crazy_Leroys.dbconfig.inc');
     include $_SERVER['DOCUMENT_ROOT'].'connect_to_mysql.php';
     //$con = mysqli_connect();
-    $con = mysqli_connect($db_host, $db_username, $db_pass, $db_name);
+    $con = mysql_connect($db_host, $db_username, $db_pass, $db_name);
     if (!$con) {
         echo "<br>";
         echo "Unable to connect to database! Please try again later. Error message: " . mysqli_error($con);
@@ -18,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "<br>";
         echo "connection made with db";
     }
-    mysqli_select_db($con, $schema);
+    //mysql_select_db($con, $schema);
 //    $myusername = mysqli_real_escape_string($con, $_POST['username']);
 //    $mypassword = mysqli_real_escape_string($con, $_POST['password']);
 
