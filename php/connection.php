be4623acec00ee6441adeb2a31a7fc2e0f2a11da
Erik Storla <estorla@spsu.edu>
2014-11-15 17:45:19 -0500

    working on loginService

--- php/connection.php
+++ php/connection.php
@@ -1,15 +1,23 @@
 <?php
 
 function initializeConnection() {
-    $host = 'swe3613.com';
-    $user = 'wapp02p2swe3613';
-    $pass = '345dfg567dss';
-    $schema = 'swe3613_db02p2';
+$host = 'swe3613.com';
+$user = 'wapp02p2swe3613';
+$pass = '345dfg567dss';
+$schema = 'swe3613_db02p2';
 
-    $con = mysqli_connect($host, $user, $pass, $schema);
+$con = mysqli_connect($host, $user, $pass, $schema);
 
-    if ($con->connect_error) {
-        die('<p>$con->connect_errno: $con->connect_error</p>');
-    }
-    return $con;
+if ($con->connect_error) {
+die('<p>$con->connect_errno: $con->connect_error</p>');
 }
+return $con;
+}
+
+$db_host = "ftp.swe3613.com";
+$db_username = "wapp02p2swe3613";
+$db_pass = "345dfg567dss";
+$db_name = "swe3613_db02p2";
+
+mysqli_connect("$db_host", "$db_username", "$db_pass") or die(mysql_error());
+mysqli_select_db("$db_name") or die("no database by that name");
