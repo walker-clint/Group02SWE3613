5b8bb49ee26a0467b2f1bef1ffa5028d2f60c5ce
Erik Storla <estorla@spsu.edu>
2014-11-15 12:01:39 -0500

    login.php, session instead of post

--- login.php
+++ login.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<?php 
-		require $_SERVER['DOCUMENT_ROOT'] . '/_page/headLinks.php';
-        
+    <head>
+        <?php
+        require $_SERVER['DOCUMENT_ROOT'] . '/_page/headLinks.php';
+
         $errorMsg = "";
         session_start();
         include_once "connect_to_mysql.php";
-        if ($_POST['user_name']) {
+        //if ($_POST['user_name']) {
+        if($_SESSION['id']){
             //Connect to the database through our include
             $user_name = ereg_replace("[^A-Za-z0-9]", "", $_POST['user_name']);
             $password = ereg_replace("[^A-Za-z0-9]", "", $_POST['password']); // filter everything but numbers and letters
@@ -22,73 +23,72 @@
                     // Get member username into a session variable
                     $user_name = $row["login"];
                     $_SESSION['login'] = $user_name;
-					$is_admin = $row["admin"];
-                    $_SESSION['is_admin'] = $is_admin;//checks if user is an administrator or regular user
-				    header("location: index.php");
+                    $is_admin = $row["admin"];
+                    $_SESSION['is_admin'] = $is_admin; //checks if user is an administrator or regular user
+                    header("location: index.php");
                     exit();
-                    
                 } // close while
             } else {
                 $errorMsg .= "The username or password you entered is incorrect<br />";
             }
         }// close if post
- // Close else after database duplicate field value checks
-            // Close else after missing vars check
-         //Close if $_POST
+        // Close else after database duplicate field value checks
+        // Close else after missing vars check
+        //Close if $_POST
         ?>
-</head>
-<body>
-<!--Start Header-->
-<?php require $_SERVER['DOCUMENT_ROOT'] . '/_page/header.php'; ?>
-<!--End Header--> 
+    </head>
+    <body>
+        <!--Start Header-->
+        <?php require $_SERVER['DOCUMENT_ROOT'] . '/_page/header.php'; ?>
+        <!--End Header--> 
 
-<!--Start Middle-->
-<div id="main" class="container-fluid">
+        <!--Start Middle-->
+        <div id="main" class="container-fluid">
 
-<!--Start Content-->
-<div class="row">
-<div id="left-column" class="col-sm-4"></div>
-<div id="center1-column" class="col-sm-4">
-  <div class="well bs-component"> 
-    <!--<legend>LEFT COLUMN</legend>-->
-    <h1 align="center">Login</h1>
-    <div class="well-1 bs-component">
-      <form class="form-horizontal"  method="post" name="logform" id="logform" >
-        <font color="#FF0000"><?php echo "$errorMsg"; ?></font> <br>
-        <div class="form-group">
-          <label for="user_name" class="col-lg-4 control-label">User Name</label>
-          <div class="col-lg-8">
-            <input type="text" class="form-control-1" name="user_name" placeholder="User Name">
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="password" class="col-lg-4 control-label">Password</label>
-          <div class="col-lg-8">
-            <input align="center" type="Password" class="form-control-1" name="password"
+            <!--Start Content-->
+            <div class="row">
+                <div id="left-column" class="col-sm-4"></div>
+                <div id="center1-column" class="col-sm-4">
+                    <div class="well bs-component"> 
+                        <!--<legend>LEFT COLUMN</legend>-->
+                        <h1 align="center">Login</h1>
+                        <div class="well-1 bs-component">
+                            <form class="form-horizontal"  method="post" name="logform" id="logform" >
+                                <font color="#FF0000"><?php echo "$errorMsg"; ?></font> <br>
+                                <div class="form-group">
+                                    <label for="user_name" class="col-lg-4 control-label">User Name</label>
+                                    <div class="col-lg-8">
+                                        <input type="text" class="form-control-1" name="user_name" placeholder="User Name">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="password" class="col-lg-4 control-label">Password</label>
+                                    <div class="col-lg-8">
+                                        <input align="center" type="Password" class="form-control-1" name="password"
                                                placeholder="Password">
-          </div>
-        </div>
-        <div align="center">
-          <input type="submit" value="Login"/>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-<div id="right-column" class="col-sm-4"></div>
-<!--End Middle--> 
+                                    </div>
+                                </div>
+                                <div align="center">
+                                    <input type="submit" value="Login"/>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div id="right-column" class="col-sm-4"></div>
+                <!--End Middle--> 
 
-<!--End Container--> 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
-<!--<script src="http://code.jquery.com/jquery.js"></script>--> 
-<script src="plugins/jquery/jquery-2.1.0.min.js"></script> 
-<script src="plugins/jquery-ui/jquery-ui.min.js"></script> 
-<!-- Include all compiled plugins (below), or include individual files as needed --> 
-<script src="plugins/bootstrap/bootstrap.min.js"></script> 
-<script src="plugins/justified-gallery/jquery.justifiedgallery.min.js"></script> 
-<script src="plugins/tinymce/tinymce.min.js"></script> 
-<script src="plugins/tinymce/jquery.tinymce.min.js"></script> 
-<!-- All functions for this theme + document.ready processing --> 
-<script src="js/devoops.js"></script>
-</body>
-</html>
+                <!--End Container--> 
+                <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+                <!--<script src="http://code.jquery.com/jquery.js"></script>--> 
+                <script src="plugins/jquery/jquery-2.1.0.min.js"></script> 
+                <script src="plugins/jquery-ui/jquery-ui.min.js"></script> 
+                <!-- Include all compiled plugins (below), or include individual files as needed --> 
+                <script src="plugins/bootstrap/bootstrap.min.js"></script> 
+                <script src="plugins/justified-gallery/jquery.justifiedgallery.min.js"></script> 
+                <script src="plugins/tinymce/tinymce.min.js"></script> 
+                <script src="plugins/tinymce/jquery.tinymce.min.js"></script> 
+                <!-- All functions for this theme + document.ready processing --> 
+                <script src="js/devoops.js"></script>
+                </body>
+                </html>
