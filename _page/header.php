88f72425766e33a1256ee99a7c623d21079c19d2
Erik Storla <estorla@spsu.edu>
2014-11-15 11:38:59 -0500

    header.php, changing order of header, fixing div as link

--- _page/header.php
+++ _page/header.php
@@ -1,18 +1,27 @@
-<?php
-$toplinks = "";
-session_start();
-include_once "connect_to_mysql.php";
-if ($_SESSION['id'] != "") {
-    $id = $_SESSION['id'];
-    // Put stored session variables into local php variable
-    $sql = mysql_query("SELECT * FROM tbl_user WHERE user_id='$id'");
+<header class="navbar-collapse">
 
-    while ($row = mysql_fetch_array($sql)) {
-        // Use the AYAH object to see if the user passed or failed the game.
-        // Get member ID into a session variable
-        $full_name = $row["first_name"] . " " . $row["last_name"];
-    } // close while
-    $toplinks = '<ul class="nav navbar-nav pull-right panel-menu">
+
+    <div id="logo" class="col-xs-6 col-sm-6">
+        <a href="index.php"> <img src="img/cllogo_medium.png" class="img-responsive"></a>
+    </div>
+
+    <div id="top-panel" class="container-fluid expanded-panel"> <br>
+
+        <?php
+        $toplinks = "";
+        session_start();
+        include_once "connect_to_mysql.php";
+        if ($_SESSION['id'] != "") {
+            $id = $_SESSION['id'];
+            // Put stored session variables into local php variable
+            $sql = mysql_query("SELECT * FROM tbl_user WHERE user_id='$id'");
+
+            while ($row = mysql_fetch_array($sql)) {
+                // Use the AYAH object to see if the user passed or failed the game.
+                // Get member ID into a session variable
+                $full_name = $row["first_name"] . " " . $row["last_name"];
+            } // close while
+            $toplinks = '<ul class="nav navbar-nav pull-right panel-menu">
 	<li class="btn-label-right">
 	<div class="well-1 btn">
 	<a href="main_menu.php">' . $full_name . '</a>
@@ -23,8 +32,8 @@ if ($_SESSION['id'] != "") {
 	<a href="logout.php">Log Out</a>
 	</li>
 	</ul>';
-} else {
-    $toplinks = '<ul class="nav navbar-nav pull-right panel-menu">
+        } else {
+            $toplinks = '<ul class="nav navbar-nav pull-right panel-menu">
 	<li class="btn-label-right">
 	<div class="well-1 btn">
 	<a href="login.php">Login</a>
@@ -36,16 +45,11 @@ if ($_SESSION['id'] != "") {
 	</div>
 	</li>
 	</ul>';
-}
-?>
-<header class="navbar-collapse">
+        }
+        ?>
 
-
-    <div id="logo" class="col-xs-6 col-sm-6">
-        <a href="index.php"> <img src="img/cllogo_medium.png" class="img-responsive"/></a>
-    </div>
-
-    <div id="top-panel" class="container-fluid expanded-panel"> <br>
-        <div class="row"><?php echo $toplinks; ?></div>
+        <div class="row">
+            <?php echo $toplinks; ?>
+        </div>
     </div>
 </header>
