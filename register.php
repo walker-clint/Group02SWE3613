ec589000228d64d0e60f25dc99e18b863219593c
Clint Walker <walker.clint@icloud.com>
2014-11-15 12:29:12 -0500

    26

--- register.php
+++ register.php
@@ -134,8 +134,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                         echo recaptcha_get_html($publickey);
                                         ?>
                                     </div>
+                                    <br>
                                     <div align="center">
-                                        <input type="button" value="Submit"/>
+                                        <input type="button-" value="Submit"/>
                                     </div>
                                 </div>
                             </form>
@@ -146,7 +147,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
         <div id="right-column" class="col-sm-2"></div>
 
+    </div>
+    <!--End Content--> 
 
+</div>
+<!--End Middle--> 
+
+<!--End Container--> 
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<!--<script src="http://code.jquery.com/jquery.js"></script>--> 
+<script src="plugins/jquery/jquery-2.1.0.min.js"></script> 
+<script src="plugins/jquery-ui/jquery-ui.min.js"></script> 
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="plugins/bootstrap/bootstrap.min.js"></script> 
+<script src="plugins/justified-gallery/jquery.justifiedgallery.min.js"></script> 
+<script src="plugins/tinymce/tinymce.min.js"></script> 
+<script src="plugins/tinymce/jquery.tinymce.min.js"></script> 
+<!-- All functions for this theme + document.ready processing --> 
+<script src="js/devoops.js"></script>
+</body>
+</html>
         <!--                        <h1>Register</h1>
                                 <div class="well-1 bs-component">
         
@@ -212,23 +232,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                     </form>
                                 </div>-->
 
-    </div>
-    <!--End Content--> 
 
-</div>
-<!--End Middle--> 
-
-<!--End Container--> 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
-<!--<script src="http://code.jquery.com/jquery.js"></script>--> 
-<script src="plugins/jquery/jquery-2.1.0.min.js"></script> 
-<script src="plugins/jquery-ui/jquery-ui.min.js"></script> 
-<!-- Include all compiled plugins (below), or include individual files as needed --> 
-<script src="plugins/bootstrap/bootstrap.min.js"></script> 
-<script src="plugins/justified-gallery/jquery.justifiedgallery.min.js"></script> 
-<script src="plugins/tinymce/tinymce.min.js"></script> 
-<script src="plugins/tinymce/jquery.tinymce.min.js"></script> 
-<!-- All functions for this theme + document.ready processing --> 
-<script src="js/devoops.js"></script>
-</body>
-</html>
