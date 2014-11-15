541c932e796b6ff93ed960482e91b6cd09dc2fac
Erik Storla <estorla@spsu.edu>
2014-11-15 12:17:45 -0500

    lockService.php, whitespace

--- php/lockService.php
+++ php/lockService.php
@@ -1,16 +1,15 @@
 <?php
+
 include('connection.php');
 session_start();
-$user_check=$_SESSION['login_user'];
+$user_check = $_SESSION['login_user'];
 
-$ses_sql=mysqli_query($db,"select username from admin where username='$user_check' ");
+$ses_sql = mysqli_query($db, "select username from admin where username='$user_check' ");
 
-$row=mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+$row = mysqli_fetch_array($ses_sql, MYSQLI_ASSOC);
 
-$login_session=$row['username'];
+$login_session = $row['username'];
 
-if(!isset($login_session))
-{
-header("Location: index.html");
+if (!isset($login_session)) {
+    header("Location: index.html");
 }
-?>
