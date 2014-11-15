472a27daa3e7081ebe366fb3f5292c2ee18a251c
Clint Walker <walker.clint@icloud.com>
2014-11-15 11:59:23 -0500

    15

--- register.php
+++ register.php
@@ -95,27 +95,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                     <input type="text" class="form-control" name="firstname" placeholder="First Name" value="<?php echo "$firstname"; ?>">
                                 </div>
                                 <label for="lastname" class="col-xs-2 col-md-2 control-label">Last Name</label>
-                                <div class="col-xs-10">
+                                <div class="col-xs-10 col-md-10">
                                     <input type="text" class="form-control" name="lastname" placeholder="Last Name" value="<?php echo "$lastname"; ?>">
                                 </div>
                                 <label for="email" class="col-xs-2 col-md-2 control-label">Email</label>
-                                <div class="col-lg-10">
+                                <div class="col-xs-10 col-md-10">
                                     <input type="email" class="form-control" name="email" placeholder="Email" value="<?php echo "$email"; ?>">
                                 </div>
                                 <label for="username" class="col-xs-2 col-md-2 control-label">Username</label>
-                                <div class="col-lg-10">
+                                <div class="col-xs-10 col-md-10">
                                     <input type="text" class="form-control" name="username" placeholder="Username" value="<?php echo "$username"; ?>">
                                 </div>
                                 <label for="password" class="col-xs-2 col-md-2 control-label">Password</label>
-                                <div class="col-lg-10">
+                                <div class="col-xs-10 col-md-10">
                                     <input type="password" class="form-control" name="password" placeholder="Password" value="<?php echo "$password"; ?>">
                                 </div>
                                 <label for="secret_q" class="col-xs-2 col-md-2 control-label">Secret Question</label>
-                                <div class="col-lg-10">
+                                <div class="col-xs-10 col-md-10">
                                     <input type="text" class="form-control" name="secret_q" placeholder="Secret Question" value="<?php echo "$secret_q"; ?>">
                                 </div>
                                 <label for="secret_a" class="col-xs-2 col-md-2 control-label">Secret Answer</label>
-                                <div class="col-lg-10">
+                                <div class="col-xs-10 col-md-10">
                                     <input type="text" class="form-control" name="secret_a" placeholder="Secret Answer" value="<?php echo "$secret_a"; ?>">
                                 </div>
                             </form>
@@ -123,7 +123,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 
-                        <h1>Register</h1>
+<!--                        <h1>Register</h1>
                         <div class="well-1 bs-component">
 
                             <form  align="center" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
@@ -173,7 +173,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <!--                  <label class="col-lg-4 control-label">Captcha</label>-->
+                                                      <label class="col-lg-4 control-label">Captcha</label>
                                     <div class="col-lg-8">
                                         <?php
                                         require_once('recaptchalib.php');
@@ -185,7 +185,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                     <input type="submit" value="Submit"/>
                                 </div>
                             </form>
-                        </div>
+                        </div>-->
                     </div>
 
                     <div id="right-column" class="col-sm-2"></div>
