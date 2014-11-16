b2a0457cfe9818918f7d358f10ab98427c4584f7
Erik Storla <estorla@spsu.edu>
2014-11-15 20:46:35 -0500

    playSong, cleaning up
    removing old code and making more readable

--- js/playSong.js
+++ js/playSong.js
@@ -1,7 +1,7 @@
 function changeBox(songLink) {
     alert("test");
     var element = document.getElementById("vidWindow")
-    var link = '<iframe width = "350" height = "280" src = "//www.youtube.com/embed/' + songLink + '?autoplay=1" frameborder = "0" allowfullscreen> </iframe>';
-    //var link = songLink;
+    var link = '<iframe width = "350" height = "280" src = "//www.youtube.com/embed/'
+            + songLink + '?autoplay=1" frameborder = "0" allowfullscreen> </iframe>';
     element.innerHTML = link;
 }
