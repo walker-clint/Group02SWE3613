8a80fa528e311eb70ed0ae12951c2ac4c78aa7d6
Erik Storla <estorla@spsu.edu>
2014-11-15 20:48:54 -0500

    playSong, changing alert
    instead of "test" will pop-up the link of the song to be played

--- js/playSong.js
+++ js/playSong.js
@@ -1,5 +1,5 @@
 function changeBox(songLink) {
-    alert("test");
+    alert("Playing "+songLink);
     var element = document.getElementById("vidWindow")
     var link = '<iframe width = "350" height = "280" src = "//www.youtube.com/embed/'
             + songLink + '?autoplay=1" frameborder = "0" allowfullscreen> </iframe>';
