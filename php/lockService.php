1975cc83658b60a63a95ad5d984e7b5f3ecdc826
Erik Storla <estorla@spsu.edu>
2014-11-15 19:33:14 -0500

    lockService, fixing bad SQL Query

--- php/lockService.php
+++ php/lockService.php
@@ -7,11 +7,11 @@ $user_check = $_SESSION['user_id'];
 
 $con = initializeConnection();
 
-$ses_sql = mysqli_query($con, "select username from admin where username= '$user_check' ");
+$ses_sql = mysqli_query($con, "select admin from tbl_user where login = '$user_check' ");
 
 $row = mysqli_fetch_array($ses_sql, MYSQLI_ASSOC);
 
-$login_session = $row['login'];
+$login_session = $row['admin'];
 
 if (!isset($login_session)) {
     header('Location: http://' . $_SERVER['SERVER_NAME'] . '/login.php');
