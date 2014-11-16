bf5c62142c63127d79611700e03757f8ff2ec751
Erik Storla <estorla@spsu.edu>
2014-11-16 02:31:41 -0500

    testCallingPage, testing Song->getInfoBox

--- _test/testCallingPage.php
+++ _test/testCallingPage.php
@@ -6,8 +6,11 @@
     </head>
     <body>
         <?php
+        include_once $_SERVER['DOCUMENT_ROOT'] . '/php/queries.php';
         echo basename($_SERVER['PHP_SELF']);
-        echo '<br>This: '.strpos(basename($_SERVER['PHP_SELF']),'te');
-       ?>
+        echo '<br>This: ' . strpos(basename($_SERVER['PHP_SELF']), 'te');
+        $song = getSong(21)->getInfoBox();
+        echo '<br>' . $song;
+        ?>
     </body>
 </html>
