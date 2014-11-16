8f4108d7cebe44b0c0383cfdfc3e5f28fdab95fa
Erik Storla <estorla@spsu.edu>
2014-11-15 19:25:27 -0500

    logoutService, adding comments

--- php/logoutService.php
+++ php/logoutService.php
@@ -1,8 +1,7 @@
 <?php
-
+//destroy the session and direct to the home page
 session_start();
 if (session_destroy()) {
-    //header("Location: " . $_SERVER['DOCUMENT_ROOT'] . "/index.php");
     header('Location: http://' . $_SERVER['SERVER_NAME'] . '/index.php');
 }
 ?>
