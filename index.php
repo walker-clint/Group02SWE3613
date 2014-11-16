4c5102f974f78b69683b83d950be7ecabfc10cfb
madeyoso66 <adeyosoyemic@gmail.com>
2014-11-15 22:34:29 -0500

    index copy from index2

--- index.php
+++ index.php
@@ -10,47 +10,55 @@
         <!--End Header-->
         <!--Start Middle-->
         <div id="main" class="container-fluid">
-
-
-            <!--Start Content-->
+			<!--Start Content-->
             <div class="row">
                 <div id="left-column" class="col-sm-4">
-                    <h1>Name and artist of song playing. Even if the</h1>
                     <div class="well bs-component">
+                        <!--<legend>LEFT COLUMN</legend>-->
+                        <h1>Name and artist of song playing. Even if the</h1>
+
+                        <div class="form-horizontal" action="" method="POST"></div>
+
                         <div class="well-1 bs-component">
-                            <div class="video-container">
-                                <div class="video-container" id="vidWindow">
+                            <div class="video-container" id="vidWindow">
                                 <!-- to autoplay in the src="//www.youtube.com/embed/...?autoplay" the ... is the link #= ... and this is the number we need to get and fill from YouTube -->
                                 <iframe width="350" height="280" src="//www.youtube.com/embed/WUdIKdRuYc4?autoplay=0" frameborder="0" allowfullscreen></iframe>
 
                             </div><p id="songInfo">abcd</p>
-                            </div>
                         </div>
                     </div>
                 </div>
+
                 <div id="right-column" class="col-sm-8">
-                    <h1>Top Ten Songs</h1>
                     <div class="well bs-component">
+                        <!--<legend>RIGHT COLUMN</legend>-->
+                        <h1>Top Ten Songs</h1>
+
+                        <div class="form-horizontal" action="" method="POST"></div>
+
                         <div class="well-1 bs-component">
-                           <table class="table">
+                            <table class="table">
                                 <thead>
                                     <tr>
                                         <td><b>Title</b></td>
 
                                         <td><b>Artist</b></td>
                                     </tr>
-                                    </thead>
+                                          </thead>
                                 <tbody>
-                                    <?php echo '<script src="http://' . $_SERVER['SERVER_NAME'] . '/js/songFunctions.js"></script>';
+                                    <?php echo '<script src="http://' . $_SERVER['SERVER_NAME'] . '/js/songFunctions.js"></script>' ?>
+
+                                    <?php
                                     include_once $_SERVER['DOCUMENT_ROOT'] . '/php/queries.php';
                                     include_once $_SERVER['DOCUMENT_ROOT'] . '/php/objects.php';
 
-                                    $mixTapeList = getAllSongs();
+                                     $mixTapeList = getBestMixTape();
 
                                     foreach ($mixTapeList as $song) {
                                         if ($song instanceof Song) {
                                             $songTitle = $song->title;
                                             $songArtist = $song->getArtists();
+                                            $songGenre = $song->getGenres();
                                             $songLink = $song->getLink();
 
                                             echo '<tr>';
@@ -60,19 +68,20 @@
                                         }
                                     }
                                     ?>
+
+                          
+                                    <!--Start well-1-->
+
+                                    <?php
+                                    ?>
                                 </tbody>
                             </table>
-                            <!--                            <div class="form-horizontal" action="">
-                                                            <input class="btn btn-primary btn-lg" type="submit" value="Song Information" name="song_info">
-                                                        </div>-->
 
                             <!--End well-1-->
                         </div>
                     </div>
-                </div>
 
-            </div>
-            <!--End Content-->
+                </div>
 
         </div>
         <!--End Middle-->
