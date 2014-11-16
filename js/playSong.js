bcccbc94884b2d44745077c410045c7fb587f58c
Erik Storla <estorla@spsu.edu>
2014-11-15 21:17:32 -0500

    playSong.js delete
    renamed and moved into songFunctions.js

--- js/playSong.js
+++
@@ -1,7 +0,0 @@
-function changeBox(songLink) {
-    alert("Playing "+songLink);
-    var element = document.getElementById("vidWindow")
-    var link = '<iframe width = "350" height = "280" src = "//www.youtube.com/embed/'
-            + songLink + '?autoplay=1" frameborder = "0" allowfullscreen> </iframe>';
-    element.innerHTML = link;
-}
