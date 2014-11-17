f738765018ef3c388863103171e2f1864cfc6112
Erik Storla <estorla@spsu.edu>
2014-11-17 12:07:38 -0500

    index, making song <div>s clickable
    clicking a song's div changes the song box

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
                                 }
                                 ?>
                                 <!--<iframe width="350" height="280" src="//www.youtube.com/embed/WUdIKdRuYc4?autoplay=0" frameborder="0" allowfullscreen></iframe>-->
-                                <!--<iframe width="350" height="280" src="//www.youtube.com/embed/<?php // echo $initialSong;   ?>?autoplay=0" frameborder="0" allowfullscreen></iframe>-->
+                                <!--<iframe width="350" height="280" src="//www.youtube.com/embed/<?php // echo $initialSong;       ?>?autoplay=0" frameborder="0" allowfullscreen></iframe>-->
                             </div><p id="songInfo"></p>
                         </div>
                     </div>
@@ -64,8 +64,9 @@
                                     $songGenre = $song->getGenres();
                                     $songLink = $song->getLink();
 
-                                    echo '<tr><td><h1>' . $increment . '</h1></td><td><div class="well-1 bs-component">' .
-                                    $song->getTitleArtist_InfoBox() . '</div></td><tr>'; //' by '.$song->getArtists().'</div></td><tr>';
+                                    echo '<tr><td><h1>' . $increment . '</h1></td><td><div class="well-1 bs-component"'
+                                    . 'onclick="' . $song->getJavascript_changeBox() . '" onmouseover="" style="cursor: pointer;">'
+                                    . $song->getTitleArtist_InfoBox() . '</div></td><tr>'; //' by '.$song->getArtists().'</div></td><tr>';
                                 }
                                 $increment+=1;
                             }
