d33652f6a50e0dc0eea1f28e82423fc0b182144a
Erik Storla <estorla@spsu.edu>
2014-11-18 10:56:47 -0500

    songFunctions, adding admin versions

--- js/songFunctions.js
+++ js/songFunctions.js
@@ -47,3 +47,50 @@ function changeInfo(title, artist, genre) {
             + '<br>Genre: ' + genre;
     element.innerHTML = info;
 }
+
+/**
+ * Changes the information in the infobox and displays/plays its youtube video
+ * 
+ * @param String title the title of the song
+ * @param String artist the artist(s) of the song
+ * @param String genre the genre(s) of the song
+ * @param String songLink the youtube key for the song ex: "hX3O5v-ylC4"
+ */
+
+function changeBox_admin(title, artist, genre, songLink) {
+    //alert("Playing " + songLink);
+    changeInfo(title, artist, genre);
+    changeVideo(songLink);
+}
+
+/**
+ * displays and autoplays a youtube video in the HTML element with id="vidWindow_admin"
+ * @param {type} songLink
+ */
+
+function changeVideo_admin(songLink) {
+    var element = document.getElementById("vidWindow_admin");
+    var link = 'No Youtube video for this song! :(';
+
+    if (songLink.length > 0) {
+        link = '<iframe width = "350" height = "280" src = "//www.youtube.com/embed/'
+                + songLink + '?autoplay=1" frameborder = "0" allowfullscreen> </iframe>';
+
+    }
+    element.innerHTML = link;
+}
+
+/**
+ * changes the song info in the HTML element with id="songInfo_admin"
+ * @param {type} title
+ * @param {type} artist
+ * @param {type} genre
+ */
+
+function changeInfo_admin(title, artist, genre) {
+    var element = document.getElementById("songInfo_admin");
+    var info = 'Title: ' + title
+            + '<br>Artist: ' + artist
+            + '<br>Genre: ' + genre;
+    element.innerHTML = info;
+}
