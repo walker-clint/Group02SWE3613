d32f7ac9cdd182f5284a6cabe17bed65e7d6526c
Erik Storla <estorla@spsu.edu>
2014-11-18 13:55:07 -0500

    menu_functions, cleaning up
    removing white space, adding comment

--- php/menu_functions.php
+++ php/menu_functions.php
@@ -5,25 +5,22 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/php/queries.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/php/setQueries.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/php/objects.php';
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($_POST['actionType'] == 'toggleFlag') {
         $songId = $_POST['songId'];
         $song = getSong($songId);
-        if (empty($song)) {
-            echo "No song found!";
-        }
         $flag = 0;
         if ($song->flagged == 0) {
             $flag = 1;
         }
-        //updateSong($id, $title, $approved, $flagged, $youtubeLink, $youtubeApproved)
         $title = $song->title;
         $approved = $song->approved;
         $youtubeLink = $song->youtubeLink;
         $youtubeApproved = $song->youtubeApproved;
+
         updateSong($songId, $title, $approved, $flag, $youtubeLink, $youtubeApproved);
-        //}
     }
 }
+
+//send user to admin page, lockService will redirect as needed
 header('Location: http://' . $_SERVER['SERVER_NAME'] . '/admin_main_menu.php');
