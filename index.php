e39851c12e2e0a4ec193eeeba0a4b89ad39289d6
Clint Walker <walker.clint@icloud.com>
2014-11-19 10:43:10 -0500

    2

--- index.php
+++ index.php
@@ -50,19 +50,21 @@
             <div class="well bs-component">
                 <div class="panel panel-default" align="left">
                     <div class="panel panel-body">
-                        <h3>You Ad here!!</h3>
+                        <div align="center">
+                            <h3>You Ad here!!</h3>
+                        </div>
 
                         <div id="logo" class="col-xs-12 col-sm-12"><img
                                 src="img/Comic_Characters_Painter_clip_art_medium.png" class="img-responsive"/>
                         </div>
                     </div>
                     <div class="panel panel-footer">
-                        <a1>Image Credit:
-                            <a1 href="http://vector.me/browse/132175/people_man_artist_painter_comic_characters_painters"
+                        <a1>Image Credit: <a
+                                href="http://vector.me/browse/132175/people_man_artist_painter_comic_characters_painters"
                                 title="People Man Artist Painter Comic Characters Painters" target="_blank">People Man
-                                Artist Painter Comic Characters Painters from
-                            </a1>
-                            <a1 href="http://vector.me/" title="Vector.me"></a1>
+                                Artist Painter Comic Characters Painters</a> from <a href="http://vector.me"
+                                                                                     title="Vector.me" target="_blank">Vector.me</a>
+                            (by nicubunu)
                         </a1>
                     </div>
                 </div>
