63143b95c4fc9e3dc2f11a7651d01a21ac9a072d
Erik Storla <estorla@spsu.edu>
2014-11-19 15:19:48 -0500

    creating footer file

---
+++ _page/footer.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div align="center">
+
+        <p class="text-muted">Group 02p2 Project 2 SWE3613 Southern Polytechnic State Univerisity (SPSU)</p>
+    </div>
+    <div align="center">
+
+        <p class="text-muted">Copy Right 2014: Clinton Walker; Erik Storla; Michael Adeyosoye</p>
+    </div>
+</div>
