6fec1db3ab615964a9756b7032873d499017d151
Clint Walker <walker.clint@icloud.com>
2014-11-19 10:37:55 -0500

    tagging

--- index.php
+++ index.php
@@ -56,15 +56,12 @@
                     </div>
                     <div class="panel panel-footer">
                         <a1>Image Credit:
-                            <a1
-                                href="http://vector.me/browse/132175/people_man_artist_painter_comic_characters_painters"
-                                title="People Man Artist Painter Comic Characters Painters" target="_blank">People
-                                Man
-                                Artist Painter Comic Characters Painters
-                            </a1>
+                            <a1 href="http://vector.me/browse/132175/people_man_artist_painter_comic_characters_painters"
+                                title="People Man Artist Painter Comic Characters Painters" target="_blank">People Man
+                                Artist Painter Comic Characters Painters />
                             from
                             <a1 href="http://vector.me/"
-                                title="Vector.me"
+                                title="Vector.me" />
                         </a1>
                     </div>
                 </div>
