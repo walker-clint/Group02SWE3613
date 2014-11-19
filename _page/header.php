72b847955ecffddfbbeea6866283e05b7eacfaaa
Clint Walker <walker.clint@icloud.com>
2014-11-19 11:52:01 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	_page/header.php
    
    tabs

--- _page/header.php
+++ _page/header.php
@@ -1,9 +1,9 @@
 <header class="navbar-collapse">
     <div id="logo" class="col-xs-6 col-sm-6">
-        <a href="index.php"><img src="img/cllogo_medium.png" class="img-responsive" /></a>
+        <a href="index.php"><img src="img/cllogo_medium.png" class="img-responsive"/></a>
     </div>
 
-    <div id="top-panel" class="container-fluid expanded-panel"> <br>
+    <div id="top-panel" class="container-fluid expanded-panel"><br>
 
         <?php
         $toplinks = "";
@@ -11,7 +11,7 @@
 
         if (!empty($_SESSION['user_id'])) {
             $id = $_SESSION['user_id'];
-            
+
             $con = initializeConnection();
             $sql = mysqli_query($con, "SELECT * FROM tbl_user WHERE user_id = '$id'");
 
@@ -25,6 +25,31 @@
             }
             
             $toplinks = '<ul class="nav navbar-nav pull-right panel-menu">
+<<<<<<< HEAD
+            <li class="btn-label-right">
+            <div class="well-1 btn">
+            <a href="main_menu.php">' . $full_name . '</a>
+            </div>
+            </li>
+            <li class="btn-label-right">
+            <div class="well-1 btn">
+            <a href="http://' . $_SERVER['SERVER_NAME'] . '/php/logoutService.php">Log Out</a>
+            </li>
+            </ul>';
+                } else {
+                    $toplinks = '<ul class="nav navbar-nav pull-right panel-menu">
+            <li class="btn-label-right">
+            <div class="well-1 btn">
+            <a href="login.php">Login</a>
+            </div>
+            </li>
+            <li class="btn-label-right">
+            <div class="well-1 btn">
+            <a href="register.php">Register</a>
+            </div>
+            </li>
+            </ul>';
+=======
 	<li class="btn-label-right">
 	<div class="well-1 btn">
 	<a href="'.$indexLink.'">' . $full_name . '</a>
@@ -48,6 +73,7 @@
 	</div>
 	</li>
 	</ul>';
+>>>>>>> origin/master
         }
         ?>
 
