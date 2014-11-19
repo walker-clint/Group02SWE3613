7ddda2aa553cfdd4b0cbb6da01f3cb5234914206
Erik Storla <estorla@spsu.edu>
2014-11-19 15:23:33 -0500

    footer, minor correction

--- _page/footer.php
+++ _page/footer.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div align="center">
 
-        <p class="text-muted">Group 02p2 Project 2 SWE3613 Southern Polytechnic State Univerisity (SPSU)</p>
+        <p class="text-muted">Group02 Project 2 SWE3613 Southern Polytechnic State Univerisity (SPSU)</p>
     </div>
     <div align="center">
 
