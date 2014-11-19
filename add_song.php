65212ec44253bd2aa0f9bfae594e31937e644280
madeyoso66 <adeyosoyemic@gmail.com>
2014-11-19 01:15:29 -0500

    autofilll test

--- add_song.php
+++ add_song.php
@@ -1,65 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <?php
+<head>
+<?php
         require $_SERVER['DOCUMENT_ROOT'] . '/_page/headLinks.php';
         ?>
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
-        <link rel="stylesheet" type="text/css"  href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" />
-         <script type="text/javascript">
-                $(document).ready(function(){
-                    $("#name").autocomplete({
-                        source:'getautocomplete.php',
-                        minLength:1
-                    });
-                });
-        </script>
-    </head>
-    <body>
-        <!--Start Header-->
-        <?php require $_SERVER['DOCUMENT_ROOT'] . '/_page/header.php'; ?>
-        <!--End Header--> 
-
-        <!--Start Middle-->
-        <div id="main" class="container-fluid">
-
-            <!--Start Content-->
-            <div class="row">
-                <div id="left-column" class="col-sm-4"></div>
-                <div id="center1-column" class="col-sm-4">
-                    <div class="well bs-component"> 
-                        <!--<legend>LEFT COLUMN</legend>-->
-                        <h1 align="center">Login</h1>
-                        <div class="well-1 bs-component">
-                           <form method="post" action="">
-           
-      </form>
-                            
-                                <div class="form-group">
-                                    <div class="col-lg-8">
-                                          Name : <input type="text" id="name" name="name" />
-                                    </div>
-                                </div>                               
-                            </form>
-                        </div>
-                    </div>
-                </div>
+</head>
+<body>
+<?php require $_SERVER['DOCUMENT_ROOT'] . '/_page/header.php'; ?>
+<div id="main" class="container-fluid">
+  <div class="row">
+    <div id="left-column" class="col-sm-4"></div>
+    <div id="center1-column" class="col-sm-4">
+      <div class="well bs-component">
+        <h1 align="center">Login</h1>
+        <div class="well-1 bs-component">
+          <form action="#" method="post">
+            <div class="form-group">
+              <label for="title" class="col-lg-4 control-label">Title</label>
+              <div class="col-lg-8">
+                <input align="center" type="text" class="form-control" id="title" name="title">
+              </div>
             </div>
-            <!--End Content-->
-            <div id="right-column" class="col-sm-4"></div>
+            <div class="form-group">
+              <label for="artist" class="col-lg-4 control-label">Artist</label>
+              <div class="col-lg-8">
+                <input align="center" type="text" class="form-control" id="artist" name="artist">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="genre" class="col-lg-4 control-label">Genre</label>
+              <div class="col-lg-8">
+                <input align="center" type="text" class="form-control" id="genre" name="genre">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="link" class="col-lg-4 control-label">Youtube Link</label>
+              <div class="col-lg-8">
+                <input align="center" type="text" class="form-control" id="link" name="link">
+              </div>
+            </div>
+          </form>
         </div>
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
-        <!--<script src="http://code.jquery.com/jquery.js"></script>--> 
-        <script src="plugins/jquery/jquery-2.1.0.min.js"></script> 
-        <script src="plugins/jquery-ui/jquery-ui.min.js"></script> 
-        <!-- Include all compiled plugins (below), or include individual files as needed --> 
-        <script src="plugins/bootstrap/bootstrap.min.js"></script> 
-        <script src="plugins/justified-gallery/jquery.justifiedgallery.min.js"></script> 
-        <script src="plugins/tinymce/tinymce.min.js"></script> 
-        <script src="plugins/tinymce/jquery.tinymce.min.js"></script> 
-        <!-- All functions for this theme + document.ready processing --> 
-        <script src="js/devoops.js"></script>
-        
-    </body>
+      </div>
+    </div>
+  </div>
+  <div id="right-column" class="col-sm-4"></div>
+</div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<!--<script src="http://code.jquery.com/jquery.js"></script>--> 
+<script src="plugins/jquery/jquery-2.1.0.min.js"></script> 
+<script src="plugins/jquery-ui/jquery-ui.min.js"></script> 
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="plugins/bootstrap/bootstrap.min.js"></script> 
+<script src="plugins/justified-gallery/jquery.justifiedgallery.min.js"></script> 
+<script src="plugins/tinymce/tinymce.min.js"></script> 
+<script src="plugins/tinymce/jquery.tinymce.min.js"></script> 
+<!-- All functions for this theme + document.ready processing --> 
+<script src="js/devoops.js"></script>
+<link rel="stylesheet" href="http://static.jquery.com/ui/css/base2.css" type="text/css" media="all" />
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js" type="text/javascript"></script>
+<script type="text/javascript">   
+
+$(document).ready(function(){
+	 var ac_config = { 
+	 source: "/ajax/demo_songs.php", 
+	 select: function(event, ui){ 
+	 $("#title").val(ui.item.title); 
+	 $("#artist").val(ui.item.artist); 
+	 $("#genre").val(ui.item.genre); 
+	 $("#link").val(ui.item.ylink); }, 
+	 minLength:1 
+	 }; 
+	 $("#city").autocomplete(ac_config);
+	  });
+	   </script>
+</body>
 </html>
---
+++ ajax/demo_songs.php
@@ -0,0 +1,58 @@
+<?php
+ include_once "connect_to_mysql.php";
+// Data could be pulled from a DB or other source
+$items = array();
+$query = "SELECT * FROM tbl_song LIMIT 5";
+$result = mysql_query($query);
+while($row = mysql_fetch_assoc($result)){
+$items[] = $row;
+} 
+
+ 
+// Cleaning up the term
+$term = trim(strip_tags($_GET['term']));
+ 
+// Rudimentary search
+$matches = array();
+foreach($items as $item){
+	if(stripos($item['title'], $term) !== false){
+		$song_id = $item['song_id'];
+		$item['ylink'] = $item['youtube'];
+		// Add the necessary "value" and "label" fields and append to result set
+		$song_genre_query = "SELECT * FROM tbl_song_genre where song_id = $song_id LIMIT 1";
+$song_genre_result = mysql_query($song_genre_query);
+while($song_genre_row = mysql_fetch_assoc($song_genre_result)){
+$genre_id = $song_genre_row['genre_id'];
+} 
+
+		$song_artist_query = "SELECT * FROM tbl_song_artist where song_id = $song_id LIMIT 1";
+$song_artist_result = mysql_query($song_artist_query);
+while($song_artist_row = mysql_fetch_assoc($song_artist_result)){
+$artist_id = $song_artist_row['artist_id'];
+} 
+
+		$genre_query = "SELECT * FROM tbl_genre where genre_id = $genre_id LIMIT 1";
+$genre_result = mysql_query($genre_query);
+while($genre_row = mysql_fetch_assoc($genre_result)){
+$item['genre'] = $genre_row['name'];
+} 
+
+		$artist_query = "SELECT * FROM tbl_artist where artist_id = $artist_id LIMIT 1";
+$artist_result = mysql_query($artist_query);
+while($artist_row = mysql_fetch_assoc($artist_result)){
+$item['artist'] = $artist_row['artist_id'];
+} 
+		
+		
+		
+		
+		
+		$item['value'] = $item['title'];
+		$item['label'] = "{$item['title']}";
+		$matches[] = $city;
+	}
+}
+ 
+// Truncate, encode and return the results
+$matches = array_slice($matches, 0, 5);
+print json_encode($matches);
