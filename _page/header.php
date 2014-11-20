1119b722ce045959166e9ae796d342558156c1bd
Clint Walker <walker.clint@icloud.com>
2014-11-20 16:52:05 -0500

    header

--- _page/header.php
+++ _page/header.php
@@ -54,6 +54,7 @@ if (!empty($_SESSION['user_id'])) {
             </ul>';
 }
 ?>
+
 <header class="navbar-collapse">
     <div id="logo" class="col-xs-6 col-sm-6">
         <a href="/<?php echo '' . $indexLink; ?>"><img src="img/cllogo_medium.png" class="img-responsive"/></a>
