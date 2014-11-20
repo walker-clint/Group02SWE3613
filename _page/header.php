58473163d8ca9a5911a2ea8112d83125305e3aeb
Clint Walker <walker.clint@icloud.com>
2014-11-20 17:57:57 -0500

    2

--- _page/header.php
+++ _page/header.php
@@ -3,6 +3,73 @@ $toplinks = "";
 require_once $_SERVER['DOCUMENT_ROOT'] . ("/php/connection.php");
 $indexLink = 'index.php';
 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//Connect to the database through our include
+
+    require_once('recaptchalib.php');
+    $privatekey = "6LcMdf0SAAAAAGoCSMb54T2MbWvgxaNpnDqhLwSj";
+    $resp = recaptcha_check_answer($privatekey, $_SERVER["REMOTE_ADDR"], $_POST["recaptcha_challenge_field"], $_POST["recaptcha_response_field"]);
+    if (!$resp->is_valid) {
+        // What happens when the CAPTCHA was entered incorrectly
+        $errorMsg .= "The reCAPTCHA wasn't entered correctly. Go back and try it again.";
+    } else {
+
+
+        include_once "connect_to_mysql.php";
+        $username = ereg_replace("[^A-Za-z0-9]", "", $_POST['username']);
+        $password = ereg_replace("[^A-Za-z0-9]", "", $_POST['password']);
+        $firstname = ereg_replace("[^A-Za-z0-9]", "", $_POST['firstname']); // filter everything but numbers and letters
+        $lastname = ereg_replace("[^A-Za-z0-9]", "", $_POST['lastname']); // filter everything but numbers and letters
+        $email = stripslashes($_POST['email']);
+        $email = strip_tags($email);
+        $email = mysql_real_escape_string($email);
+        $secret_q = ereg_replace("[^A-Za-z0-9]", "", $_POST['secret_q']);
+        $secret_a = ereg_replace("[^A-Za-z0-9]", "", $_POST['secret_a']);
+        if ((!$firstname) || (!$lastname) || (!$email) || (!$username || (!$password))) {
+
+            $errorMsg = "You did not submit the following required information!<br /><br />";
+            if (!$firstname) {
+                $errorMsg .= "--- First Name<br />";
+            } if (!$lastname) {
+                $errorMsg .= "--- Last Name<br />";
+            }if (!$email) {
+                $errorMsg .= "--- Email Address<br />";
+            }if (!$username) {
+                $errorMsg .= "--- Username<br />";
+            }if (!$password) {
+                $errorMsg .= "--- Password<br />";
+            }
+        } else {
+            $sql_username_check = mysql_query("SELECT user_id FROM tbl_user WHERE login='$username' LIMIT 1");
+            $username_check = mysql_num_rows($sql_username_check);
+            if ($username_check > 0) {
+                $errorMsg = "<u>ERROR:</u><br />The username is already in use inside our system. Please try another.";
+            } else {
+                // Add MD5 Hash to the password variable
+                $hashedPass = md5($password);
+                // Get the inserted ID here to use in the activation email
+                $id = mysql_insert_id();
+                // Add user info into the database table, claim your fields then values
+                $sql = mysql_query("INSERT INTO tbl_user (user_id,login,password,email,admin,secret_question,secret_answer,first_name, last_name)
+		VALUES('$id','$username', '$password', '$email', 0,'$secret_q','$secret_a','$firstname','$lastname')") or die(mysql_error());
+
+
+
+
+                //header("location: customeinformation.php");
+                //exit();
+
+
+
+                header("location: main_menu.php");
+                exit(); // Exit so the form and page does not display, just this success message
+            } // Close else after database duplicate field value checks
+        } // Close else after missing vars check
+    }
+} //Close if $_POST
+?>
+
 if (!empty($_SESSION['user_id'])) {
     $id = $_SESSION['user_id'];
 
@@ -83,8 +150,7 @@ if (!empty($_SESSION['user_id'])) {
         </div>
 
         <!-- Modal 1 -->
-        <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-             aria-hidden="true">
+        <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-dialog modal-vertical-centered">
                     <div class="modal-content">
@@ -139,8 +205,7 @@ if (!empty($_SESSION['user_id'])) {
             <!-- /.modal -->
 
             <!-- Modal 2 -->
-            <div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-                 aria-hidden="true">
+            <div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-dialog modal-vertical-centered">
                         <div class="modal-content">
