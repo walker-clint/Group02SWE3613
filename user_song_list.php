4b520d2afefeeb4a1a3a7a31f33e639ef11de9d0
Clint Walker <walker.clint@icloud.com>
2014-11-20 12:01:02 -0500

    User song list table data

--- user_song_list.php
+++ user_song_list.php
@@ -77,33 +77,33 @@
                     </tr>
                     </thead>
                     <tbody>
-                    <!--                        --><?php
-                    //                        $increment = 1;
-                    //                        $userMixtape = getMixtape($_SESSION['user_id']);
-                    //                        foreach ($userMixtape as $songId) {
-                    //                            $song = getSongById($songId);
-                    //                            $songTitle = $song->title;
-                    //                            $songArtist = $song->getArtists();
-                    //                            $songGenre = $song->getGenres();
-                    //                            $songLink = $song->getLink();
-                    //
-                    //                            echo '<tr><td><h1></h1>' . $songTitle . '</td><td>' . $songArtist . '</td><td>' . $songGenre . '</td><td>' . $songLink . '</td><td><div class="btn btn-primary" >Delete</div>'  . '</td><tr></tr>';
-                    //                        }
-                    ////                        foreach ($mixTapeList as $songInt) {
-                    ////                            $song = getSongById($songInt);
-                    ////                            if ($song instanceof Song) {
-                    ////                                $songTitle = $song->title;
-                    ////                                $songArtist = $song->getArtists();
-                    ////                                $songGenre = $song->getGenres();
-                    ////                                $songLink = $song->getLink();
-                    ////
-                    ////                                echo '<tr><td><h1>' . $increment . '</h1></td><td><div class="well-2 bs-component"'
-                    ////                                    . 'onclick="' . $song->js_changeBox() . '" onmouseover="" style="cursor: pointer;">'
-                    ////                                    . $song->js_infoBox() . '</div></td><tr>'; //' by '.$song->getArtists().'</div></td><tr>';
-                    ////                            }
-                    ////                            $increment += 1;
-                    ////                        }
-                    //
+                        <?php
+                        $increment = 1;
+                        $userMixtape = getMixtape($_SESSION['user_id']);
+                        foreach ($userMixtape as $songId) {
+                            $song = getSongById($songId);
+                            $songTitle = $song->title;
+                            $songArtist = $song->getArtists();
+                            $songGenre = $song->getGenres();
+                            $songLink = $song->getLink();
+
+                            echo '<tr><td><h1></h1>' . $songTitle . '</td><td>' . $songArtist . '</td><td>' . $songGenre . '</td><td>' . $songLink . '</td><td><div class="btn btn-primary" >Delete</div>'  . '</td><tr></tr>';
+                        }
+//                        foreach ($mixTapeList as $songInt) {
+//                            $song = getSongById($songInt);
+//                            if ($song instanceof Song) {
+//                                $songTitle = $song->title;
+//                                $songArtist = $song->getArtists();
+//                                $songGenre = $song->getGenres();
+//                                $songLink = $song->getLink();
+//
+//                                echo '<tr><td><h1>' . $increment . '</h1></td><td><div class="well-2 bs-component"'
+//                                    . 'onclick="' . $song->js_changeBox() . '" onmouseover="" style="cursor: pointer;">'
+//                                    . $song->js_infoBox() . '</div></td><tr>'; //' by '.$song->getArtists().'</div></td><tr>';
+//                            }
+//                            $increment += 1;
+//                        }
+
                     ?>
                     <?php ?>
                     </tbody>
