808f372d9f771018721869313b34229ae1178605
Erik Storla <estorla@spsu.edu>
2014-11-20 09:11:16 -0500

    footer, copyright disclaimer

--- _page/footer.php
+++ _page/footer.php
@@ -6,5 +6,7 @@
     <div align="center">
 
         <p class="text-muted">Copy Right 2014: Clinton Walker; Erik Storla; Michael Adeyosoye</p>
+        <p class="text-muted">No media is hosted on this site or by "Crazy Leroy"<br>any DMCA or similar complaints should be filed with Youtube</p>
+
     </div>
 </div>
