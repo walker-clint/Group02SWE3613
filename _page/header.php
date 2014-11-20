f72af596578ec4813e964ab05423b3608d7cc021
Erik Storla <estorla@spsu.edu>
2014-11-20 08:47:39 -0500

    header, changing logo link
    header now links to the appropriate page depending on the user's access
    (index, main_menu, or admin_main_menu)

--- _page/header.php
+++ _page/header.php
@@ -1,30 +1,23 @@
-<header class="navbar-collapse">
-    <div id="logo" class="col-xs-6 col-sm-6">
-        <a href="index.php"><img src="img/cllogo_medium.png" class="img-responsive"/></a>
-    </div>
+<?php
+$toplinks = "";
+require_once $_SERVER['DOCUMENT_ROOT'] . ("/php/connection.php");
+$indexLink='index.php';
 
-    <div id="top-panel" class="container-fluid expanded-panel"><br>
+if (!empty($_SESSION['user_id'])) {
+    $id = $_SESSION['user_id'];
 
-        <?php
-        $toplinks = "";
-        require_once $_SERVER['DOCUMENT_ROOT'] . ("/php/connection.php");
+    $con = initializeConnection();
+    $sql = mysqli_query($con, "SELECT * FROM tbl_user WHERE user_id = '$id'");
 
-        if (!empty($_SESSION['user_id'])) {
-            $id = $_SESSION['user_id'];
-
-            $con = initializeConnection();
-            $sql = mysqli_query($con, "SELECT * FROM tbl_user WHERE user_id = '$id'");
-
-            $full_name = '';
-            while ($row = mysqli_fetch_array($sql)) {
-                $full_name = "Welcome: " . $row["first_name"] . " " . $row["last_name"];
-            }
-            $indexLink = 'main_menu.php';
-            if ($userType == 'admin') {
-                $indexLink = 'admin_main_menu.php';
-            }
-
-            $toplinks = '<ul class="nav navbar-nav pull-right panel-menu">
+    $full_name = '';
+    while ($row = mysqli_fetch_array($sql)) {
+        $full_name = $row["first_name"] . " " . $row["last_name"];
+    }
+    $indexLink = 'main_menu.php';
+    if ($userType == 'admin') {
+        $indexLink = 'admin_main_menu.php';
+    }
+    $toplinks = '<ul class="nav navbar-nav pull-right panel-menu">
             <li class="btn-label-right">
             <div class="well-1 btn">
             <a href="' . $indexLink . '">' . $full_name . '</a>
@@ -41,8 +34,8 @@
             <a href="http://' . $_SERVER['SERVER_NAME'] . '/php/logoutService.php">Log Out</a>
             </li>
             </ul>';
-        } else {
-            $toplinks = '<ul class="nav navbar-nav pull-right panel-menu">
+} else {
+    $toplinks = '<ul class="nav navbar-nav pull-right panel-menu">
             <li class="btn-label-right">
             <div class="well-1 btn">
             <a href="login.php">Login</a>
@@ -59,9 +52,14 @@
             </div>
             </li>
             </ul>';
-        }
-        ?>
+}
+?>
+<header class="navbar-collapse">
+    <div id="logo" class="col-xs-6 col-sm-6">
+        <a href="/<?php echo '' . $indexLink; ?>"><img src="img/cllogo_medium.png" class="img-responsive"/></a>
+    </div>
 
+    <div id="top-panel" class="container-fluid expanded-panel"><br>
         <div class="row">
             <?php echo $toplinks; ?>
         </div>
