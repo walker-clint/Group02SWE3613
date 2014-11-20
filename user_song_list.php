f3d2decedaed8041b91114f6894f588d33b9fa27
Clint Walker <walker.clint@icloud.com>
2014-11-20 12:21:56 -0500

    Revert "4"
    This reverts commit 0196ec8721dd6f71fdd863e6607503aeaab347e1.

--- user_song_list.php
+++ user_song_list.php
@@ -98,9 +98,9 @@
                         $songArtist = $song->getArtists();
                         $songGenre = $song->getGenres();
                         $songLink = $song->getLink();
-//                        <h4>' . $increment . '</h4>
-                        echo '<tr><th></th><th></th><th>' . $songTitle . '</th><th>' . $songLink  . '</th><th>'
-                            . $songGenre . '</th></tr><tr><th></th><th>' . $songArtist . '</th><th></th><th> <div class="btn btn-primary" >Delete</div>' . '</td></tr>';
+
+                        echo '<tr><th><h4>' . $increment . '</h4></th><th></th><th>' . $songTitle . '</th><th>' . $songLink  . '</th><th>'
+                            . $songGenre . '</th></tr><tr><th>' . $songArtist . '</th><th></th><th> <div class="btn btn-primary" >Delete</div>' . '</td></tr>';
                         $increment += 1;
                     }
                     //                        foreach ($mixTapeList as $songInt) {
