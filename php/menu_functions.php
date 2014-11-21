787f68897b00e5fe45e06528004601e22d1003ff
Erik Storla <estorla@spsu.edu>
2014-11-21 13:15:49 -0500

    menuFunctions, fix for Approve and Unflag not working
    wasn't getting song's current status

--- php/menu_functions.php
+++ php/menu_functions.php
@@ -8,6 +8,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/php/objects.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($_POST['actionType'] == 'toggleFlag') {
         $songId = $_POST['songId'];
+        $song = getSongById($songId);
         $flag = 1;
         if ($song->flagged == 1 && $userType == 'admin') {
             $flag = 0;
@@ -15,6 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         updateSongFlagged($songId, $flag);
     } else if ($_POST['actionType'] == 'toggleApproval' && $userType == 'admin') {
         $songId = $_POST['songId'];
+        $song = getSongById($songId);
         $approve = 0;
         if ($song->approved == 0) {
             $approve = 1;
