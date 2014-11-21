93e58093b0b1c7b54f51cee81ea2d10763492b27
Erik Storla <estorla@spsu.edu>
2014-11-20 20:16:55 -0500

    user_song_list, easier to read html in song loop

--- user_song_list.php
+++ user_song_list.php
@@ -103,139 +103,143 @@
                                                     $songArtist = $song->getArtists();
                                                     $songGenre = $song->getGenres();
                                                     $songLink = $song->getLink();
-                                                    echo '<tr><th></th><th><h2>' .
-                                                    $songTitle . '</h2></th><th><h2>' .
-                                                    $songArtist . '</h2></th><th><h2>' .
-                                                    $songGenre;
-                                                    ?>'</h2></th><th></th><th></th><th>
-                                                <div class="btn btn-primary" onclick="<?php echo $song->js_changeBox(true); ?>">Play Song</div>
-                                            </th><th></th><th><a href='/php/toggleMixtape.php?songId=<?php echo $songId; ?>'> <div class="btn btn-warning" >Delete</div></a>
-                                        </th><th></th></tr>
-                                        <?php
-                                    }
-                                    ?>
-                                    <?php ?>
-                                    </tbody>
-                                </table>
+                                                    ?>
+                                                    <tr>
+                                                        <th></th>
+                                                        <th><h2><?php echo $songTitle; ?></h2></th>
+                                                <th><h2><?php echo $songArtist; ?></h2></th>
+                                                <th><h2><?php echo $songGenre; ?>'</h2></th>
+                                                <th></th>
+                                                <th></th>
+                                                <th><div class="btn btn-primary" onclick="<?php echo $song->js_changeBox(true); ?>">Play Song</div></th>
+                                                <th></th>
+                                                <th><a href='/php/toggleMixtape.php?songId=<?php echo $songId; ?>'> <div class="btn btn-warning" >Delete</div></a></th><th></th>
+                                                </tr>
+                                                <?php
+                                            }
+                                            ?>
+                                            <?php ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div id="right-space" class="col-sm-1"></div>
             </div>
         </div>
-        <div id="right-space" class="col-sm-1"></div>
-    </div>
-</div>
-<!--End Row Main Page Data-->
+        <!--End Row Main Page Data-->
 
-<!--Start Ads-->
+        <!--Start Ads-->
 
-<div class="row">
-    <div id="left-column" class="col-sm-2"></div>
-    <div id="left-ad" class="col-sm-2">
-        <div class="well bs-component">
-            <div class="panel panel-default" align="left">
-                <div class="panel panel-body">
-                    <div align="center">
-                        <h3>Your Ad here!!</h3>
+        <div class="row">
+            <div id="left-column" class="col-sm-2"></div>
+            <div id="left-ad" class="col-sm-2">
+                <div class="well bs-component">
+                    <div class="panel panel-default" align="left">
+                        <div class="panel panel-body">
+                            <div align="center">
+                                <h3>Your Ad here!!</h3>
+                            </div>
+
+                            <div id="logo" class="col-xs-12 col-sm-12">
+                                <img src="img/Comic_Characters_Painter_clip_art_medium.png" class="img-responsive"/>
+                            </div>
+                        </div>
+                        <div class="panel panel-footer">
+                            <a1>Image Credit: <a
+                                    href="http://vector.me/browse/132175/people_man_artist_painter_comic_characters_painters"
+                                    title="People Man Artist Painter Comic Characters Painters" target="_blank">People Man
+                                    Artist Painter Comic Characters Painters</a> from <a href="http://vector.me"
+                                                                                     title="Vector.me" target="_blank">Vector.me</a>
+                                (by nicubunu)
+                            </a1>
+                        </div>
                     </div>
-
-                    <div id="logo" class="col-xs-12 col-sm-12">
-                        <img src="img/Comic_Characters_Painter_clip_art_medium.png" class="img-responsive"/>
-                    </div>
-                </div>
-                <div class="panel panel-footer">
-                    <a1>Image Credit: <a
-                            href="http://vector.me/browse/132175/people_man_artist_painter_comic_characters_painters"
-                            title="People Man Artist Painter Comic Characters Painters" target="_blank">People Man
-                            Artist Painter Comic Characters Painters</a> from <a href="http://vector.me"
-                                                                             title="Vector.me" target="_blank">Vector.me</a>
-                        (by nicubunu)
-                    </a1>
                 </div>
             </div>
-        </div>
-    </div>
-    <div id="center-ad" class="col-sm-4">
-        <div class="well bs-component">
-            <div class="panel panel-default" align="left">
-                <div class="panel panel-body">
-                    <div align="center">
-                        <h3>Your Ad here!!</h3>
-                    </div>
+            <div id="center-ad" class="col-sm-4">
+                <div class="well bs-component">
+                    <div class="panel panel-default" align="left">
+                        <div class="panel panel-body">
+                            <div align="center">
+                                <h3>Your Ad here!!</h3>
+                            </div>
 
-                    <div id="logo" class="col-xs-12 col-sm-12"><img
-                            src="img/Comic_Characters_Santa_clip_art_medium.png" class="img-responsive"/>
+                            <div id="logo" class="col-xs-12 col-sm-12"><img
+                                    src="img/Comic_Characters_Santa_clip_art_medium.png" class="img-responsive"/>
+                            </div>
+                        </div>
+                        <div class="panel panel-footer">
+                            <a1>Image Credit: <a href="http://vector.me/browse/153534/comic_characters_santa_clip_art"
+                                                 title="Comic Characters Santa Clip Art" target="_blank">Comic Characters
+                                    Santa Clip Art</a> from <a href="http://vector.me" title="Vector.me" target="_blank">Vector.me</a>
+                                (by nicubunu)
+                            </a1>
+                        </div>
                     </div>
-                </div>
-                <div class="panel panel-footer">
-                    <a1>Image Credit: <a href="http://vector.me/browse/153534/comic_characters_santa_clip_art"
-                                         title="Comic Characters Santa Clip Art" target="_blank">Comic Characters
-                            Santa Clip Art</a> from <a href="http://vector.me" title="Vector.me" target="_blank">Vector.me</a>
-                        (by nicubunu)
-                    </a1>
                 </div>
             </div>
-        </div>
-    </div>
-    <div id="right-ad" class="col-sm-2">
-        <div class="well bs-component">
-            <div class="panel panel-default" align="left">
-                <div class="panel panel-body">
-                    <div align="center">
-                        <h3>Your Ad here!!</h3>
-                    </div>
+            <div id="right-ad" class="col-sm-2">
+                <div class="well bs-component">
+                    <div class="panel panel-default" align="left">
+                        <div class="panel panel-body">
+                            <div align="center">
+                                <h3>Your Ad here!!</h3>
+                            </div>
 
-                    <div id="logo" class="col-xs-12 col-sm-12"><img
-                            src="img/Comic_Characters_Painter_clip_art_medium.png" class="img-responsive"/>
+                            <div id="logo" class="col-xs-12 col-sm-12"><img
+                                    src="img/Comic_Characters_Painter_clip_art_medium.png" class="img-responsive"/>
+                            </div>
+                        </div>
+                        <div class="panel panel-footer">
+                            <a1>Image Credit: <a
+                                    href="http://vector.me/browse/132175/people_man_artist_painter_comic_characters_painters"
+                                    title="People Man Artist Painter Comic Characters Painters" target="_blank">People Man
+                                    Artist Painter Comic Characters Painters</a> from <a href="http://vector.me"
+                                                                                     title="Vector.me" target="_blank">Vector.me</a>
+                                (by nicubunu)
+                            </a1>
+                        </div>
                     </div>
-                </div>
-                <div class="panel panel-footer">
-                    <a1>Image Credit: <a
-                            href="http://vector.me/browse/132175/people_man_artist_painter_comic_characters_painters"
-                            title="People Man Artist Painter Comic Characters Painters" target="_blank">People Man
-                            Artist Painter Comic Characters Painters</a> from <a href="http://vector.me"
-                                                                             title="Vector.me" target="_blank">Vector.me</a>
-                        (by nicubunu)
-                    </a1>
                 </div>
             </div>
+            <div id="left-column" class="col-sm-2"></div>
         </div>
+
+        <!--End Ads-->
+
     </div>
-    <div id="left-column" class="col-sm-2"></div>
-</div>
 
-<!--End Ads-->
+    <!--footer-->
+    <footer class="footer">
+        <div class="container">
+            <div align="center">
 
-</div>
+                <p class="text-muted">Group 02p2 Project 2 SWE3613 Southern Polytechnic State Univerisity (SPSU)</p>
+            </div>
+            <div align="center">
 
-<!--footer-->
-<footer class="footer">
-    <div class="container">
-        <div align="center">
-
-            <p class="text-muted">Group 02p2 Project 2 SWE3613 Southern Polytechnic State Univerisity (SPSU)</p>
+                <p class="text-muted">Copy Right 2014: Clinton Walker; Erik Storla; Michael Adeyosoye</p>
+            </div>
         </div>
-        <div align="center">
-
-            <p class="text-muted">Copy Right 2014: Clinton Walker; Erik Storla; Michael Adeyosoye</p>
-        </div>
-    </div>
-</footer>
+    </footer>
 
 
-<!--End Container-->
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<!--<script src="http://code.jquery.com/jquery.js"></script>-->
-<script src="plugins/jquery/jquery-2.1.0.min.js"></script>
-<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="plugins/bootstrap/bootstrap.min.js"></script>
-<script src="plugins/justified-gallery/jquery.justifiedgallery.min.js"></script>
-<script src="plugins/tinymce/tinymce.min.js"></script>
-<script src="plugins/tinymce/jquery.tinymce.min.js"></script>
-<!-- All functions for this theme + document.ready processing -->
-<script src="js/devoops.js"></script>
+    <!--End Container-->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!--<script src="http://code.jquery.com/jquery.js"></script>-->
+    <script src="plugins/jquery/jquery-2.1.0.min.js"></script>
+    <script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="plugins/bootstrap/bootstrap.min.js"></script>
+    <script src="plugins/justified-gallery/jquery.justifiedgallery.min.js"></script>
+    <script src="plugins/tinymce/tinymce.min.js"></script>
+    <script src="plugins/tinymce/jquery.tinymce.min.js"></script>
+    <!-- All functions for this theme + document.ready processing -->
+    <script src="js/devoops.js"></script>
 
 
 </body>
