2c63b39e75bbad056b889a71a2d451a19884548f
Clint Walker <walker.clint@icloud.com>
2014-11-21 10:12:08 -0500

    added get URL function to queries.php

--- php/queries.php
+++ php/queries.php
@@ -461,3 +461,19 @@ function getMixtapeSortPosition($userIDInc) {
     }
     return $returnArray;
 }
+
+/**
+ * returns URL of current page.
+ * found @ http://webcheatsheet.com/php/get_current_page_url.php
+ * */
+function curPageURL() {
+    $pageURL = 'http';
+    if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+    $pageURL .= "://";
+    if ($_SERVER["SERVER_PORT"] != "80") {
+        $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+    } else {
+        $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+    }
+    return $pageURL;
+}
