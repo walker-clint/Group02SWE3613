5a47c54a02c9daac8ad71bb9a2a6ac2857c0de6d
Erik Storla <estorla@spsu.edu>
2014-11-21 23:45:36 -0500

    liveSearch, should return "Add" button

--- php/liveSearchResult.php
+++ php/liveSearchResult.php
@@ -1,5 +1,4 @@
 <?php
-
 include_once $_SERVER['DOCUMENT_ROOT'] . '/php/queries.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/php/objects.php';
 if ($_POST) {
@@ -10,7 +9,11 @@ if ($_POST) {
         $songList = getSongsBySearch($q);
     }
     foreach ($songList as $song) {
-        echo '<tr><td>'.$song->js_infoBox(true) . '</td></tr>';
+        ?><a href='/php/toggleMixtape.php?songId=<?php echo $song->id; ?>'>
+            <div class="btn btn-warning">Add</div>
+        </a>
+        <?php
+        echo $song->js_infoBox(true);
+        echo '<tr><td>' . $song->js_infoBox(true) . '</td></tr>';
     }
 }
-        
