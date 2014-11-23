2e583fc8c7dece70e6c1533e8e03f4a06adb1745
Erik Storla <estorla@spsu.edu>
2014-11-23 15:59:17 -0500

    feedback on song flag/approval

--- php/menu_functions.php
+++ php/menu_functions.php
@@ -11,7 +11,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $song = getSongById($songId);
         $flag = 1;
         if ($song->flagged == 1 && $userType == 'admin') {
+            $_SESSION['error']='"'.$song->title.'" is no longer flagged.';
             $flag = 0;
+        }else{
+            $_SESSION['error']='"'.$song->title.'" has been flagged, an admin will review it.';
         }
         updateSongFlagged($songId, $flag);
     } else if ($_POST['actionType'] == 'toggleApproval' && $userType == 'admin') {
@@ -20,6 +23,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $approve = 0;
         if ($song->approved == 0) {
             $approve = 1;
+            $_SESSION['error']='"'.$song->title.'" is now approved.';
+        }else{
+            $_SESSION['error']='"'.$song->title.'" is no longer approved.';
         }
         updateSongApproved($songId, $approve);
     }
