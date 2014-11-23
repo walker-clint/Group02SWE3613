07ea4298bbbcc0c051df07430d6a2de7749ec3e8
Erik Storla <estorla@spsu.edu>
2014-11-23 18:40:41 -0500

    header, commentis

--- _page/header.php
+++ _page/header.php
@@ -2,47 +2,50 @@
 /**
  * The header loaded on all pages of the site.
  */
-
 $display_name = "";
 //$toplinks = "";
 require_once $_SERVER['DOCUMENT_ROOT'] . ("/php/connection.php");
-$indexLink = 'index.php';//default the "Index" or "Home" button to this var
-$page = getCurrentPageURL();//get the name of the loading page
+$indexLink = 'index.php'; //default the "Index" or "Home" button to this var
+$page = getCurrentPageURL(); //get the name of the loading page
 
-$buttons = array();//array of buttons to display on the page
-
+$buttons = array(); //array of buttons to display on the page
 //usable buttons, more are below after some values are set
-$buttonSongList = '<li class="btn-label-right"><a href="user_song_list.php">'
-        . '<div class="well-1 btn btn-primary">Your Song List</div></a></li>';
-$buttonLogin = ' <li class="btn-label-right"><a data-toggle="modal" href="#myModal1">'
-        . '<div class="well-1 btn btn-primary">Login</div></a></li>';
+
+//the register button, opens registration modal, for non-logged in users
 $buttonRegister = '<li class="btn-label-right"><a data-toggle="modal" href="#myModal2">'
         . '<div class="well-1 btn btn-primary">Register</div></a></li>';
 
+//the login button, opens the login modal, for non-logged in users
+$buttonLogin = ' <li class="btn-label-right"><a data-toggle="modal" href="#myModal1">'
+        . '<div class="well-1 btn btn-primary">Login</div></a></li>';
+
+//directs to the user's song list, for logged in users
+$buttonSongList = '<li class="btn-label-right"><a href="user_song_list.php">'
+        . '<div class="well-1 btn btn-primary">Your Song List</div></a></li>';
+
+//the logout button, logs out the user, for logged in users
+$buttonLogOut = '<li class="btn-label-right"><a href="http://' . $_SERVER['SERVER_NAME'] . '/php/logoutService.php">'
+        . '<div class="well-1 btn btn-warning">Log Out</div></a></li>';
+
 if (!empty($_SESSION['user_id'])) {//if logged in
-    $id = $_SESSION['user_id'];//get the user's id so we can get their name
-
-//    $con = initializeConnection();
-//    $sql = mysqli_query($con, "SELECT * FROM tbl_user WHERE user_id = '$id'");
-
+    $id = $_SESSION['user_id']; //get the user's id so we can get their name
     $full_name = $_SESSION['full_name'];
-//    while ($row = mysqli_fetch_array($sql)) {
-//        $full_name = $row["first_name"] . " " . $row["last_name"];
-//    }
-    $indexLink = 'main_menu.php';
+    $indexLink = 'main_menu.php'; //at this point the user is a registered user, set indexLink
 
     if ($userType == 'admin') {
-        $indexLink = 'admin_main_menu.php';
+        $indexLink = 'admin_main_menu.php'; //if they are an admin link to admin page
     }
 
-    //these buttons need values set to work
+    //these buttons need $indexLink set to work
+    
+    //the home button, goes to the user's index page
     $buttonHome = '<li class="btn-label-right"><a href="' . $indexLink . '">'
             . '<div class="well-1 btn btn-primary">HOME</div></a></li>';
-    $buttonLogOut = '<li class="btn-label-right"><a href="http://' . $_SERVER['SERVER_NAME'] . '/php/logoutService.php">'
-            . '<div class="well-1 btn btn-warning">Log Out</div></a></li>';
+    //the refresh button, goes to the user's index page
     $buttonRefresh = '<li class="btn-label-right"><a href="' . $indexLink . '">'
             . '<div class="well-1 btn btn-primary">Refresh Page</div></a></li>';
 
+    //the following if's set which buttons a user will see
     if ($page == "http://group02p2.swe3613.com/user_song_list.php") {
         if (!empty($full_name)) {
             $display_name = '<div align="center"> <h3>' . "Rock On! : " . $full_name . '</h3> </div>';
@@ -71,6 +74,7 @@ if (!empty($_SESSION['user_id'])) {//if logged in
                 <?php echo $display_name; ?>
                 <ul class="nav navbar-nav pull-right panel-menu">
                     <?php
+                    //display the buttons set above
                     foreach ($buttons as $button) {
                         echo $button;
                     }
@@ -80,6 +84,7 @@ if (!empty($_SESSION['user_id'])) {//if logged in
         </div>
 
         <?php
+        //This displays any feedback messages to the user in a pop-up box
         if (!empty($_SESSION['error'])) {
             ?><div class = "row">
                 <div id = "center1-column" class = "col-sm-4"></div>
@@ -96,6 +101,7 @@ if (!empty($_SESSION['user_id'])) {//if logged in
 
         <!-- Modal 1 -->
         <?php
+        //we only load the login and register modals if a users isn't logged in
         if (empty($_SESSION['user_id'])) {
             require_once $_SERVER['DOCUMENT_ROOT'] . ("/_page/MyModal1.php");
             ?>
