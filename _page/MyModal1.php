df7b7d81f1b90c3a349ee74efe70a25ca04ea455
madeyoso66 <adeyosoyemic@gmail.com>
2014-12-01 17:23:48 -0500

    modal

--- _page/MyModal1.php
+++ _page/MyModal1.php
@@ -1,4 +1,4 @@
-<!--MyModal1, the login modal-->
+<!--MyModal2, the registration modal-->
 <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
      aria-hidden="true">
     <div class="modal-dialog">
@@ -9,47 +9,17 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <div align="center">
-                        <div class="well bs-component">
-                            <h1 align="center">Login</h1>
-
-                            <div class="well-2 bs-component">
-                                <form class="form-horizontal" action="/php/loginService.php" method="POST">
-                                    <div class="form-group">
-                                        <label for="user_name" class="col-sm-4 col-md-4 control-label">User
-                                            Name</label>
-
-                                        <div class="col-sm-8 col-md-8">
-                                            <input type="text" class="form-control-1" id="username"
-                                                   name="username" placeholder="User Name" value='<?php // echo "$myusername"       ?>' required>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="password"
-                                               class="col-sm-4 col-md-4 control-label">Password</label>
-
-                                        <div class="col-sm-8 col-md-8">
-                                            <input type="Password" class="form-control-1"
-                                                   id="password" name="password"
-                                                   placeholder="Password" required>
-                                        </div>
-                                    </div>
-                                    <div align="center">
-                                        <input class="btn btn-primary" type="submit" value="Login"/>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" data-dismiss="modal">Close
-                        </button>
-                    </div>
+                    <?php require_once $_SERVER['DOCUMENT_ROOT'] . ("/_page/loginForm.php"); ?>
+                    <!--End Content-->
                 </div>
-                <!-- /.modal-content -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Close
+                    </button>
+                </div>
             </div>
+            <!-- /.modal-content -->
         </div>
-        <!-- /.modal-dialog -->
     </div>
-    <!-- /.modal -->
+    <!-- /.modal-dialog -->
 </div>
+<!-- /.modal -->
