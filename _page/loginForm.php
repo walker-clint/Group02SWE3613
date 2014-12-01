9fc1c7d51784016eb62905f9745a7064a5c68041
madeyoso66 <adeyosoyemic@gmail.com>
2014-12-01 17:26:06 -0500

    loginForm fix

--- _page/loginForm.php
+++ _page/loginForm.php
@@ -1,34 +1,32 @@
 <!--Registration form-->
-<div align="center">
-    <!--Start Content-->
-    <form class="form-horizontal" action="login.php" method="POST">
-        <h1>Registration</h1>
 
-        <div class="row">
-           <div class="well bs-component"> 
-                        <!--<legend>LEFT COLUMN</legend>-->
-                        <h1 align="center">Login</h1>
-                        <div class="well-1 bs-component">
-                            <form class="form-horizontal" action="php/loginService.php" method="POST">
-                                <div class="form-group">
-                                    <label for="user_name" class="col-lg-4 control-label">User Name</label>
-                                    <div class="col-lg-8">
-                                        <input type="text" class="form-control" id="username" name="username" placeholder="User Name" value='<?php echo "$myusername" ?>' required>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="password" class="col-lg-4 control-label">Password</label>
-                                    <div class="col-lg-8">
-                                        <input align="center" type="Password" class="form-control" id="password" name="password"
+<div align="center"> 
+  <!--Start Content-->
+  <form class="form-horizontal" action="login.php" method="POST">
+    <h1>Registration</h1>
+    <div class="row">
+      <div class="well bs-component"> 
+        <!--<legend>LEFT COLUMN</legend>-->
+        <h1 align="center">Login</h1>
+        <div class="well-1 bs-component">
+          <div class="form-group">
+            <label for="user_name" class="col-lg-4 control-label">User Name</label>
+            <div class="col-lg-8">
+              <input type="text" class="form-control" id="username" name="username" placeholder="User Name" value='<?php echo "$myusername" ?>' required>
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="password" class="col-lg-4 control-label">Password</label>
+            <div class="col-lg-8">
+              <input align="center" type="Password" class="form-control" id="password" name="password"
                                                placeholder="Password" required>
-                                    </div>
-                                </div>
-                                <div align="center">
-                                    <input class="btn btn-primary" type="submit" value="Login"/>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+            </div>
+          </div>
+          <div align="center">
+            <input class="btn btn-primary" type="submit" value="Login"/>
+          </div>
         </div>
-    </form>
+      </div>
+    </div>
+  </form>
 </div>
